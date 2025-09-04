9c5e4874a5ae6a7daf96cc93394f59bf3f15d8a0
Nicolás Jares <nico.jar@gmail.com>
2025-09-03 22:57:57 -0300

    ejemplos de norma 1

--- codigos/p1/ej9.py
+++ codigos/p1/ej9.py
@@ -11,3 +11,20 @@ def g_aprox_gauss_legendre(x,f,h,n):
 
 def f_diff_gauss_legendre(x,f,h,n):
 	return (3/( 2*h ))*g_aprox_gauss_legendre(x,f,h,n)
+
+# Ejemplos de formas de calcular las normas para los errores
+# Por ejemplo, para el tercer error
+# f: la función sin ruido
+# df: la derivada exacta de f
+
+x = np.linspace(-1,1,100)
+
+# Error como la suma de desviaciones, es un múltiplo de la norma 1
+E_3 = np.sum(np.abs(df(x) - f_diff_gauss_legendre(x, f, h, n)))
+
+# Norma 1 con quad
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.quad.html
+from scipy.integrate import quad
+
+diff = lambda x : np.abs(df(x) - f_diff_gauss_legendre(x, f, h, n))
+E_3 = quad(diff, -1, 1)[0]
